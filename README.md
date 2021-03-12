43700eae13816c8c8d9f4fda3522bb57c0924a0d
Volkan Kumbasar <7557299+kumbasar@users.noreply.github.com>
2021-03-12 20:52:03 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# portfolios
+# BIST Portfolios
+
+## Install requirements
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+```bash
+./main.py
+```
+
+Sampe output:
+```bash
+
+****
+GUBRF
+Cost: 85.3 TRY
+Mean: 80.62 TRY
+Max: 82.55 TRY
+Min: 79.65 TRY
+
+             Open   High    Low  Close    Volume Currency
+Date                                                     
+2021-03-10  81.10  85.65  80.75  82.55  13190867      TRY
+2021-03-12  79.35  81.70  78.60  79.65   3088867      TRY
+
+****
+SISE
+Cost: 7.1 TRY
+Mean: 7.61 TRY
+Max: 7.73 TRY
+Min: 7.13 TRY
+
+            Open  High   Low  Close     Volume Currency
+Date                                                   
+2021-02-26  7.08  7.19  7.04   7.13  113245600      TRY
+2021-03-12  7.72  7.73  7.61   7.66   77813600      TRY
+```
---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import investpy
+import datetime
+import pprint
+import json
+import pandas as pd
+
+country = 'Turkey'
+date_frmt = '%d/%m/%Y'
+today = datetime.datetime.today().strftime(date_frmt)
+
+
+def getStocks(stock_file='stocks.json'):
+    with open(stock_file, 'r') as json_file:
+        return json.load(json_file)
+
+
+if __name__ == "__main__":
+
+    stocks = getStocks()
+
+    for stk in stocks:
+        df = investpy.get_stock_historical_data(stock=stk['code'],
+                                                country=country,
+                                                from_date=stk['date'],
+                                                to_date=today)
+
+        stk_mean = df["Close"].mean()
+        stk_max = df["Close"].max()
+        stk_min = df["Close"].min()
+        df_temp = pd.concat([df.head(1), df.tail(1)])
+
+        print("\n****\n{}".format(stk['code']))
+        print("Cost: {} TRY".format(stk['price']))
+        print("Mean: {} TRY".format(round(stk_mean, 2)))
+        print("Max: {} TRY".format(stk_max))
+        print("Min: {} TRY\n".format(stk_min))
+        print(df_temp)
